6edfcf3a8307e0f746a8a565a5a1364847603254
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 23:15:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,46 +1,53 @@
 class TicTacToe
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
+
+  def initialize(board = Array.new(9," "))
+    @board = board
   end
 
   WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
   ]
 
   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def move(index, current_player = "X")
-    @board[index - 1] = current_player
+  def input_to_index(user_input)
+    user_input.to_i-1
   end
 
-  def position_taken?(location)
-    @board[location] != " " && @board[location] != ""
+  def move(index, player = X)
+    index = input.to_i - 1
+    @board[index] = player
   end
 
-  def valid_move?(index)
-    index = index.to_i - 1
-    index.between?(0,8) && !position_taken?(index.to_i)
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(input)
+    index = input.to_i - 1
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input.to_i
-    if valid_move?(index)
-      move(index, current_player)
+    index = input.to_i - 1
+    if valid_move?(input)
+      player = current_player
+      move(input, player)
       display_board
     else
       turn
@@ -48,63 +55,71 @@ class TicTacToe
   end
 
   def turn_count
-    @board.count {|token| token == "X"|| token == "O"}
+    count = 0
+    @board.each do |slot|
+      if slot == "X" || slot == "O"
+        count += 1
+      end
+    end
+    count
   end
 
   def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+    if turn_count % 2 == 0
+      "X"
+    else
+      "O"
+    end
   end
 
   def won?
-
-    WIN_COMBINATIONS.each do |win_combo|
-      if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") || (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
-        return win_combo
+    win = false
+    WIN_COMBINATIONS.each do |wincombination|
+      if @board[wincombination[0]] == "X" && @board[wincombination[1]] == "X" && @board[wincombination[2]] == "X"
+        win = wincombination
+      elsif @board[wincombination[0]] == "O" && @board[wincombination[1]] == "O" && @board[wincombination[2]] == "O"
+        win = wincombination
       end
     end
-    return false
+    win
   end
 
-  #ternary syntax
   def full?
-    @board.all? {|full| full == "X" || full == "O"} ? true : false
+    @board.all? do |slot|
+      slot == "X" || slot == "O"
+    end
   end
 
   def draw?
-    if full? == true && won?  == false
-      return true
-    else
-      return false
+    draw = false
+    full = full?
+    won = won?
+    if full == true && won == false
+      draw = true
     end
   end
 
   def over?
-    if draw? == true || full? == true || won? == true
-      return true
-    else
-      false
+    over = false
+    if won? != false || draw? == true
+      over = true
     end
   end
 
   def winner
-    return nil if !won?
-    position = won?[0]
-      if @board[position] == "X"
-        return "X"
-      elsif @board[position] == "O"
-        return "O"
+    if won? != nil
+    winner = @board[won?[0]]
     end
   end
 
   def play
-    until over?
+    until over? == true
       turn
     end
-      if won?
-        puts "Congratulations #{winner}!"
-      elsif draw?
-        puts "Cats Game!"
-      end
+    if won? != false
+      puts "Congratulations #{winner}!"
+    elsif draw? == true
+      puts "Cat's Game!"
     end
-
+  end
 end
