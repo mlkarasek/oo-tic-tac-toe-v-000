7cd11e4821c9a41a9ec9bbecfca83ab212dac56e
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 22:43:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,8 +36,9 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(input, board)
+    index = input.to_i - 1
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn
