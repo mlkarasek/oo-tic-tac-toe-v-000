32aac86e60dcd53bb0c282ffa267d085c5eb4fc3
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 22:52:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,10 +36,9 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(input)
-    index = input.to_i - 1
-    index.between?(0,8) && !position_taken?(index)
-  end
+  def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
   def turn
     puts "Please enter 1-9:"
